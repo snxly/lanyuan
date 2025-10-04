f09a88e6d07b9e364b43c72ea9e23f8a0e8cd5ca
Luying Xu <ssnxly@gmail.com>
2025-10-04 16:37:58 +0800

    ucharts demo works

--- miniprogram/pages/dashboard/dashboard.js
+++ miniprogram/pages/dashboard/dashboard.js
@@ -1,4 +1,7 @@
 // pages/dashboard/dashboard.js
+import uCharts from '../../lib/u-charts.min.js';
+var uChartsInstance = {};
+
 const app = getApp()
 
 Page({
@@ -43,6 +46,7 @@ Page({
               dashboardData: res.data.data,
               loading: false
             })
+            this.drawCharts('trend')
           }
           resolve()
         },
@@ -64,5 +68,44 @@ Page({
     wx.switchTab({
       url: '/pages/records/records'
     })
-  }
+  },
+
+  drawCharts(id){
+    const data = {
+      categories: ["2016","2017","2018","2019","2020","2021"],
+      series: [
+        {
+          name: "目标值",
+          data: [35,36,31,33,13,34]
+        },
+        {
+          name: "完成量",
+          data: [18,27,21,24,6,28]
+        }
+      ]
+    };
+    const ctx = wx.createCanvasContext(id, this);
+    uChartsInstance[id] = new uCharts({
+        type: "column",
+        context: ctx,
+        width: 750 / 750 * wx.getSystemInfoSync().windowWidth,
+        height: 500 / 750 * wx.getSystemInfoSync().windowWidth,
+        categories: data.categories,
+        series: data.series,
+        animation: true,
+        background: "#FFFFFF",
+        padding: [15,15,0,5],
+        xAxis: {
+          disableGrid: true
+        },
+        yAxis: {
+          data: [{min: 0}]
+        },
+        extra: {
+          column: {
+            type: "group"
+          }
+        }
+      });
+  },
 })

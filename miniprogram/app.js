21c747c83edab01ad27274ee0ab0e1f88061089a
Luying Xu <ssnxly@gmail.com>
2025-10-05 00:16:08 +0800

    wx.cloud

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,9 +2,21 @@
 App({
   onLaunch() {
     // 小程序启动时执行
+    if (wx.cloud) {
+      wx.cloud.init({
+        // 替换为你自己的云环境ID
+        env: 'prod-0gmg3xtse9cdc95b',
+        // 建议开启，方便在云控制台追踪用户
+        traceUser: true
+      })
+    }
   },
 
   globalData: {
-    apiBaseUrl: 'http://192.168.46.72:3000/api' // 后端API地址
+    // 后端API地址
+    // apiBaseUrl: 'http://192.168.46.72:3000/api' 
+    apiBaseUrl: 'https://lanyuan-backend-190986-4-1381571830.sh.run.tcloudbase.com/api',
+    // todo: 微信如何区分线上环境还是测试环境？
+    test: false,
   }
 })
--- miniprogram/pages/dashboard/dashboard.js
+++ miniprogram/pages/dashboard/dashboard.js
@@ -38,45 +38,72 @@ Page({
     this.setData({ loading: true })
 
     return new Promise((resolve, reject) => {
-      wx.request({
-        url: `${app.globalData.apiBaseUrl}/dashboard`,
-        success: (res) => {
-          if (res.data.success) {
-            this.setData({
-              dashboardData: res.data.data,
-              loading: false
-            })
-            const dailyData = res.data.data.dailyData
-            const {dates, dailyCounts, trendCounts} = dailyData
-            const totalTrendData = {
-              dates: dates,
-              counts: [{
-                name: '2024',
-                data: trendCounts,
-              }]
-            }
-            const dailyTrendData = {
-              dates: dates,
-              counts: [{
-                name: '2024',
-                data: dailyCounts,
-              }]
-            }
-            this.drawCharts('trend', 'line', totalTrendData)
-            this.drawCharts('daily', 'column', dailyTrendData)
-          }
-          resolve()
-        },
-        fail: (err) => {
-          console.error('获取数据失败:', err)
-          this.setData({ loading: false })
-          wx.showToast({
-            title: '获取数据失败',
-            icon: 'none'
+      const success = (res) => {
+        if (res.data.success) {
+          this.setData({
+            dashboardData: res.data.data,
+            loading: false
           })
-          reject(err)
+          const dailyData = res.data.data.dailyData
+          const {dates, dailyCounts, trendCounts} = dailyData
+          const totalTrendData = {
+            dates: dates,
+            counts: [{
+              name: '2024',
+              data: trendCounts,
+            }]
+          }
+          const dailyTrendData = {
+            dates: dates,
+            counts: [{
+              name: '2024',
+              data: dailyCounts,
+            }]
+          }
+          this.drawCharts('trend', 'line', totalTrendData)
+          this.drawCharts('daily', 'column', dailyTrendData)
         }
-      })
+        resolve()
+      }
+      const fail = (err) => {
+        console.error('获取数据失败:', err)
+        this.setData({ loading: false })
+        wx.showToast({
+          title: '获取数据失败',
+          icon: 'none'
+        })
+        reject(err)
+      }
+      const testMode = app.globalData.test == true
+      if (testMode) {
+        console.log('=== test mode')
+        wx.request({
+          url: `${app.globalData.apiBaseUrl}/dashboard`,
+          success,
+          fail,
+        })
+      } else {
+        console.log('=== prod mode')
+        const request = wx.cloud.callContainer({
+          "config": {
+            "env": "prod-0gmg3xtse9cdc95b"
+          },
+          "path": "/api/dashboard",
+          "header": {
+            "X-WX-SERVICE": "lanyuan-backend",
+            "content-type": "application/json"
+          },
+          "method": "GET",
+          "data": ""
+        })
+        request.then((res)=> {
+          if (res.data.success) {
+            success(res)
+          }
+        }).catch((err) => {
+          fail(err)
+        })
+      }
     })
   },
 

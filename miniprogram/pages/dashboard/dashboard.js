04ff2032579aacf28b865f00dbf1c4e7bdb19055
Luying Xu <ssnxly@gmail.com>
2025-10-04 18:31:12 +0800

    update charts

--- miniprogram/pages/dashboard/dashboard.js
+++ miniprogram/pages/dashboard/dashboard.js
@@ -46,8 +46,25 @@ Page({
               dashboardData: res.data.data,
               loading: false
             })
-            this.drawCharts('trend')
-            this.drawCharts('daily')
+            const dailyData = res.data.data.dailyData
+            const {dates, dailyCounts, trendCounts} = dailyData
+            const size = 6
+            const totalTrendData = {
+              dates: dates.slice(0, size),
+              counts: [{
+                name: '2024',
+                data: trendCounts.slice(0,size),
+              }]
+            }
+            const dailyTrendData = {
+              dates: dates.slice(0, size),
+              counts: [{
+                name: '2024',
+                data: dailyCounts.slice(0,size),
+              }]
+            }
+            this.drawCharts('trend', 'line', totalTrendData)
+            this.drawCharts('daily', 'column', dailyTrendData)
           }
           resolve()
         },
@@ -71,32 +88,33 @@ Page({
     })
   },
 
-  drawCharts(id){
-    const data = {
-      categories: ["2016","2017","2018","2019","2020","2021"],
-      series: [
-        {
-          name: "目标值",
-          data: [35,36,31,33,13,34]
-        },
-        {
-          name: "完成量",
-          data: [18,27,21,24,6,28]
-        }
-      ]
-    };
+  drawCharts(id, type, data){
+    // const data = {
+    //   categories: ["2016","2017","2018","2019","2020","2021"],
+    //   series: [
+    //     {
+    //       name: "目标值",
+    //       data: [35,36,31,33,13,34]
+    //     },
+    //     // {
+    //     //   name: "完成量",
+    //     //   data: [18,27,21,24,6,28]
+    //     // }
+    //   ]
+    // };
     const ctx = wx.createCanvasContext(id, this);
     uChartsInstance[id] = new uCharts({
-        type: "line",
+        type,
         context: ctx,
         width: 600 / 750 * wx.getWindowInfo().windowWidth,
         height: 350 / 750 * wx.getWindowInfo().windowWidth,
-        categories: data.categories,
-        series: data.series,
+        categories: data.dates,
+        series: data.counts,
         animation: true,
         background: "#FFFFFF",
         // top, right, bottom, left
-        padding: [5,15,25,5],
+        padding: [15,15,0,5],
+        enableScroll: false,
         xAxis: {
           disableGrid: true
         },
@@ -108,6 +126,12 @@ Page({
             type: "straight",
             width: 2,
             activeType: "hollow"
+          },
+          column: {
+            type: "group",
+            width: 30,
+            activeBgColor: "#000000",
+            activeBgOpacity: 0.08
           }
         }
       });
